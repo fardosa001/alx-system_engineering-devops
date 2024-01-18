dbe04d851aca2ea1c128f2ef5d35c100ba492cf2
fardosa001 <fardosaadow@gmail.com>
2024-01-18 08:58:25 +0000

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+"""queries the Reddit API and prints the titles of the
+
+first 10 hot posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = requests.utils.default_headers()
-    headers.update({"User-Agent": "Custom User Agent"})
+    data = {}
+    headers = {"User-Agent": "Custom User Agent"}
 
-    resp = requests.get(url, headers=headers).json()
-    top_ten = resp.get('data', {}).get('children', [])
-    if not top_ten:
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+    top_ten_posts = data.get('data', {}).get('children', [])
+    if not top_ten_posts:
         print(None)
-    for top in top_ten:
-        print(top.get('data').get('title'))
-
+    for post in top_ten_posts:
+        print(post.get('data').get('title'))
