31c46128adadddf10efc2c869b1f34a17041317a
fardosa001 <fardosaadow@gmail.com>
2024-01-18 10:09:44 +0000

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,7 @@ import requests
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = requests.utils.default_headers()
-    headers.update({"User-Agent": "Custom User Agent"})
+    headers = {"User-Agent": "Custom User Agent"}
 
     resp = requests.get(url, headers=headers, allow_redirects=False).json()
     top_ten_posts = resp.get('data', {}).get('children', [])
