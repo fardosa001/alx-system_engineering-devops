9448ca5e98baed32b49372d80f20ec9aa43de686
fardosa001 <fardosaadow@gmail.com>
2024-01-20 08:16:33 +0000

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# fix an error in "phpp" extension to "php"
-# automate it using puppet
+# fix an error in "phpp" extension to "php"and automate it using puppet
 
 exec { 'fix-wordpress':
    command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-   path    => "/usr/local/bin/:/bin/"
+   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
